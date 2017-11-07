b91b9792c14c0b96a926aca904d6f4368c86b7ec
A. S. M. Khairul Alam <masul0612@gmail.com>
2017-11-07 18:15:52 +0600

    code added for assignment 4

---
+++ assignment4/reducer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+
+count = 0
+maxCount = 0
+previousFile = None
+filename = None
+
+for line in sys.stdin:
+	token = line.strip()
+
+	if token and token != previousFile:
+		if count >= maxCount:
+			maxCount = count
+			filename = previousFile
+		count = 0
+		previousFile = token
+	
+	previousFile = token
+	count += 1
+
+if count >= maxCount:
+	maxCount = count
+	filename = previousFile
+
+print maxCount
