311654f3d5b1b33ac7cbc190855279bece93be8c
A. S. M. Khairul Alam <masul0612@gmail.com>
2017-11-08 12:59:49 +0600

    code added for assignment 8

---
+++ assignment8/mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+	row = line.strip().split(" ")
+
+	if len(row) == 10
+		File = str(row[6])
+		t1 = File.strip().split(".")
+		ind = len(t1)-1
+		t2 = str(t1[ind])
+		print t2
